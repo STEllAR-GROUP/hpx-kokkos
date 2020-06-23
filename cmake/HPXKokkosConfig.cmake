cd140e29e70af49db9883e9c10175764e44d3978
Mikael Simberg <simberg@cscs.ch>
2020-06-23 16:08:13 +0200

    Require HPX 1.5.0

--- cmake/HPXKokkosConfig.cmake
+++ cmake/HPXKokkosConfig.cmake
@@ -1,7 +1,7 @@
 get_filename_component(HPXKokkos_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
-find_dependency(HPX REQUIRED)
+find_dependency(HPX 1.5.0 REQUIRED)
 find_dependency(Kokkos REQUIRED)
 
 if(NOT TARGET HPXKokkos::hpx_kokkos)
