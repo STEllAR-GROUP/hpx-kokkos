883540c4f6390b1cf1a539b967a2fa7f1b434c09
Mikael Simberg <simberg@cscs.ch>
2020-07-29 17:23:35 +0200

    Optionally include HPX CUDA headers

--- include/hpx/kokkos/future.hpp
+++ include/hpx/kokkos/future.hpp
@@ -12,8 +12,12 @@
 
 #include <hpx/kokkos/detail/logging.hpp>
 
+#include <hpx/config.hpp>
 #include <hpx/future.hpp>
+
+#if defined(HPX_WITH_CUDA)
 #include <hpx/modules/async_cuda.hpp>
+#endif
 
 #include <Kokkos_Core.hpp>
 
