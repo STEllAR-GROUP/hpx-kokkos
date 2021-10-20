a50bb7663ef91b947bcde45e8b94110e4b68e056
Mikael Simberg <mikael.simberg@iki.fi>
2021-10-20 12:33:52 +0200

    Update CDash project name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,3 +1,3 @@
-set(CTEST_PROJECT_NAME "HPX-Kokkos")
+set(CTEST_PROJECT_NAME "HPXKokkos")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 set(CTEST_SUBMIT_URL "https://cdash.cscs.ch/submit.php?project=${CTEST_PROJECT_NAME}")
