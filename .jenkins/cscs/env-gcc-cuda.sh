e6fbc9aa365431f58d9261591f5063906d660268
Mikael Simberg <mikael.simberg@iki.if>
2022-03-10 13:33:12 +0100

    Update Jenkins configurations

--- .jenkins/cscs/env-gcc-cuda.sh
+++ .jenkins/cscs/env-gcc-cuda.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 ETH Zurich
+# Copyright (c) 2020-2022 ETH Zurich
 #
 # SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -9,10 +9,11 @@ export CXX_STD="17"
 
 module load daint-gpu
 module switch PrgEnv-cray PrgEnv-gnu
-module switch gcc gcc/9.3.0
-module load cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
-module load Boost/1.75.0-CrayGNU-20.11
-module load hwloc/.2.0.3
+module load cudatoolkit
+module load Boost/1.78.0-CrayGNU-21.09
+module load hwloc/2.4.1
+spack load cmake@3.18.6
+spack load ninja@1.10.0
 
 export CXX=`which CC`
 export CC=`which cc`
