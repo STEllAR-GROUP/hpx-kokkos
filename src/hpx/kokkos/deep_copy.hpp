a0d0ff3d075c31f0b4bae1ad0b0684c5aa1f69d6
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2023-01-17 13:38:12 -0600

    Add deep_copy_async optimization for SYCL

--- src/hpx/kokkos/deep_copy.hpp
+++ src/hpx/kokkos/deep_copy.hpp
@@ -20,8 +20,26 @@ template <typename ExecutionSpace, typename... Args,
 hpx::shared_future<void> deep_copy_async(ExecutionSpace &&space,
                                          Args &&...args) {
   Kokkos::deep_copy(space, std::forward<Args>(args)...);
+  printf("normal deep copy help!!!!");
   return detail::get_future<typename std::decay<ExecutionSpace>::type>::call(
       std::forward<ExecutionSpace>(space));
 }
+#if defined(KOKKOS_ENABLE_SYCL)
+/// deep_copy_async specialization for SYCL spaces. It comes with the advantage of not having
+/// to create our own sycl::event in get_future - instead it uses the copy event directly by
+/// circumventing kokkos::deep_copy and running sycl:memcpy itself. This reduces the 
+/// overhead.
+template<typename TargetSpace, typename SourceSpace>
+hpx::shared_future<void> deep_copy_async(Kokkos::Experimental::SYCL &&instance,
+    TargetSpace &&t, SourceSpace &&s) {
+  assert(t.size() == s.size());
+  assert(q.is_in_order());
+  static_assert(std::is_same<typename std::decay<TargetSpace>::type::data_type,
+      typename std::decay<SourceSpace>::type::data_type>::value);
+  auto& q = *instance.impl_internal_space_instance()->m_queue;
+  auto event = q.memcpy(t.data(), s.data(), t.size() * sizeof(typename std::decay<TargetSpace>::type::data_type));
+  return hpx::sycl::experimental::detail::get_future(event);
+}
+#endif
 } // namespace kokkos
 } // namespace hpx
